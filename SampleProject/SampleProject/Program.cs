b21f95ae9f45e5d458e408b97ef2c18afb106cae
Yusuf Zeyrek <zeyrek.yusuf3405@gmail.com>
2021-10-02 18:15:19 +0300

    First Commit

---
+++ SampleProject/SampleProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SampleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, surname;
+                       
+            Console.Write("İsminizi giriniz: ");
+            name = Console.ReadLine();
+
+            Console.Write("Soyisminizi giriniz: ");
+            surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba {0} {1} Hoşgeldin");
+
+            Console.ReadLine();
+
+        }
+    }
+}
